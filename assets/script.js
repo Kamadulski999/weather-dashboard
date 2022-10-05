49ca496e94e56838fb0b4dd24300d35a7d16094a
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2022-10-05 11:08:42 -0500

    added forecast logic

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,7 @@
 var APIKey = "2d1eeb2d03c02cc552ff916201158e58"
+var city = "London"
+
+
 
 // https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}&units=imeperial&appid=2d1eeb2d03c02cc552ff916201158e58
   
@@ -7,14 +10,34 @@ var APIKey = "2d1eeb2d03c02cc552ff916201158e58"
 
 
 
-var forecast = function() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=London&appid=${APIKey}`).then(data => {
-      console.log(data.json());
-      })
-    
-  };
+var forecast = function(city) {
 
-  forecast()
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=imperial`)
+    .then(function(response) {
+      response.json().then(function(data) {           
+      var forecastArr = [];
+  // storing weather data from api
+  for (var i = 0; i < 5; i++) {
+      date = new Date();
+      var forecastObj = {
+          date: date,
+          weather: data.list[i].icon,
+          temp: Math.round(data.list[i].main.temp),
+          wind: data.list[i].wind.speed,
+          humidity: data.list[i].main.humidity
+      };
+      console.log(forecastObj)
+      forecastArr.push(forecastObj);
+  };
+  console.log(forecastArr);
+ 
+
+  
+   });
+});
+}
+
+  forecast(city)
 
   
  
